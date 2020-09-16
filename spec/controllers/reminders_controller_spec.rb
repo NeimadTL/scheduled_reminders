6a8fdecd35e964478017921b6e3246b14e3e328c
neimad971 <damientalbot26@gmail.com>
2020-09-16 17:42:28 +0200

    adds test for reminders_controller#update

--- spec/controllers/reminders_controller_spec.rb
+++ spec/controllers/reminders_controller_spec.rb
@@ -124,4 +124,60 @@ RSpec.describe RemindersController, type: :controller do
   end
 
 
+  describe "PUT #update" do
+    let(:new_attributes) { { title: "new title", description: "new description" } }
+    context "when user is signed in" do
+      before do
+        sign_in(user, nil)
+        Reminder.create!(valid_attributes)
+      end
+
+      context "with valid params" do
+        it "updates the requested reminder" do
+          reminder = Reminder.last
+          put :update, params: {id: reminder.id, reminder: new_attributes}, session: valid_session
+          reminder.reload
+          expect(reminder.title).to eql "new title"
+          expect(reminder.description).to eql "new description"
+        end
+
+        it "redirects to the reminder" do
+          reminder = Reminder.last
+          put :update, params: {id: reminder.id, reminder: new_attributes}, session: valid_session
+          expect(response).to redirect_to reminder_url(reminder)
+        end
+      end
+
+      context "with invalid params" do
+        it "returns a success response (i.e. to display the 'edit' template)" do
+          reminder = Reminder.last
+          put :update, params: {id: reminder.id, reminder: invalid_attributes}, session: valid_session
+          expect(response).to be_successful
+        end
+      end
+    end
+
+    context "when a user tries to update a reminder of another user" do
+      before { sign_in(another_user, nil) } # another_user signed in
+      it "returns a redirect response" do
+        reminder = Reminder.create!(valid_attributes) # reminder of user
+        put :update, params: {id: reminder.id, reminder: new_attributes}, session: valid_session
+        expect(response).to be_redirect
+        expect(response).to redirect_to(root_url)
+        expect(flash[:alert]).to match 'You are not the owner of this reminder'
+      end
+    end
+
+    context "when no user is signed in" do
+      it "returns a redirect response and redirects to sign in path" do
+        reminder = Reminder.create! valid_attributes
+        put :update, params: {id: reminder.id, reminder: new_attributes}, session: valid_session
+        expect(response).to be_redirect
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+
+  end
+
+
 end
